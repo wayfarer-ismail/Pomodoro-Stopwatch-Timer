5a7fd3f680d180dd84c3d6ffe693b10ca96e3963
wayfarer-ismail <ismailelbahja03@gmail.com>
2024-02-01 21:18:48 +0000

    added - new class to house ui components to simplify usage

--- Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
+++ Stopwatch with Productivity Timer/task/src/main/java/org/hyperskill/stopwatch/MainActivity.kt
@@ -1,9 +1,6 @@
 package org.hyperskill.stopwatch
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import org.hyperskill.stopwatch.R.*
 import org.hyperskill.stopwatch.handlers.ButtonStartReset
@@ -13,13 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_main)
 
-        val startButton = findViewById<Button>(id.startButton)
-        val resetButton = findViewById<Button>(id.resetButton)
-        val textView = findViewById<TextView>(id.textView)
-        val progressBar = findViewById<ProgressBar>(id.progressBar)
-
-        ButtonStartReset(this, startButton, resetButton, textView, progressBar)
         val uiElements = UIElements(this)
-
+        ButtonStartReset(this, uiElements)
     }
 }
